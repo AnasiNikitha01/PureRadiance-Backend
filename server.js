242649ec42ac45dd7e76ab0918f07c81b2a67e6b
Anasi Nikitha <anasi.nikitha1@gmail.com>
2023-07-01 15:18:28 +0530

    updates

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 app.use(cookieParser());
     
- app.use(cors({credentials:true,origin:'https://pure-radiance-frontend.vercel.app/'}));
+ app.use(cors({credentials:true,origin:'https://pure-radiance-frontend.vercel.app'}));
 //  app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 // app.use(cors());
 
