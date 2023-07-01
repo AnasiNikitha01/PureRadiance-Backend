cb171975b619763f2bf5bcf37f0fcbe3f43405e3
Anasi Nikitha <anasi.nikitha1@gmail.com>
2023-07-01 19:09:30 +0530

    changed

--- Routers/Logout.js
+++ Routers/Logout.js
@@ -2,11 +2,9 @@ import express from 'express';
 
 const logout = express.Router();
 
-logout.get('/logout',(req,res)=>{
-
-    console.log(reached);
-    res.cookie('token', ' ', { expires: new Date(0) });
-  res.status(200).json('Logged out successfully');
-})
+logout.get('/logout', (req, res) => {
+  // Clear the token cookie by setting an expired date
+  res.cookie('token', '', { expires: new Date(0), sameSite: 'none', secure: true }).status(200).json('Logged out successfully');
+});
 
 export default logout;
