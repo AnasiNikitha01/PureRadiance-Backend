b31b5dd3b474ddd63e457c6482eb41fcb73fcfdf
Anasi Nikitha <anasi.nikitha1@gmail.com>
2023-07-01 18:57:45 +0530

    j

--- Routers/Logout.js
+++ Routers/Logout.js
@@ -3,6 +3,8 @@ import express from 'express';
 const logout = express.Router();
 
 logout.get('/logout',(req,res)=>{
+
+    console.log(reached);
     res.cookie('token', '', { expires: new Date(0) });
   res.status(200).json('Logged out successfully');
 })
